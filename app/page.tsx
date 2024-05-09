0782bd63202d7883b36340b283eba188cc76aa02
Satyam0119 <118735981+Satyam0119@users.noreply.github.com>
2024-05-09 15:20:26 +0530

    dashboad

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import { SignIn } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
     <div className=" justify-end p-2 ">
-      <Button className="p-4 m-2 justify-end left-1" variant="gray">login </Button>
+      <Button className="p-4 m-2 justify-end left-1" variant="gray">
+        
+      </Button>
       <Button variant="red">Signup</Button>
       </div>
   );
